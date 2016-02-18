c5b1243d8cf09a34a9c0a0e1a9bf7ccdca5eff15
elizaway <elizadunaway@gmail.com>
2016-02-18 14:24:13 -0500

    Add attribute name to user model

--- db/migrate/20160217174244_create_users.rb
+++ db/migrate/20160217174244_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string   :username, null: false
+      t.string   :name, null: false
       t.string   :email, null: false
       t.string   :password_digest, null: false
 
